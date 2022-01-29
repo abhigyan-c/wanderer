bb72001cc80e7a944e52eab890971c535036670f
satatya <96868378+satatya@users.noreply.github.com>
2022-01-30 01:36:52 +0530

    Update totop.js

--- js/totop.js
+++ js/totop.js
@@ -13,10 +13,6 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  //document.body.scrollTop = 0; // For Safari
-  //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
