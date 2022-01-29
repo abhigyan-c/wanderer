344ffb88bcaa26ea974316d79f004b3197075daf
satatya <96868378+satatya@users.noreply.github.com>
2022-01-29 11:58:39 +0530

    Update totop.js

--- js/totop.js
+++ js/totop.js
@@ -4,7 +4,7 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 230 || document.documentElement.scrollTop > 230) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
