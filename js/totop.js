88ed9e73b68d2f2afdb73bdee3c707d950fd5a0f
aviiciii <laavesh1@gmail.com>
2023-02-08 16:22:57 +0530

    Formatted Javascript files

--- js/totop.js
+++ js/totop.js
@@ -1,18 +1,20 @@
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+	scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 230 || document.documentElement.scrollTop > 230) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+	if (document.body.scrollTop > 230 || document.documentElement.scrollTop > 230) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
